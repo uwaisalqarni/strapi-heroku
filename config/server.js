21c9163effb152b0096269e5904c823e19bb37c2
uwaisalqarni <qarniuwaisal565@gmail.com>
2021-05-18 13:18:36 +0700

    Update database

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('https://strapi-heroku-uaq.herokuapp.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '65e2312aabab0455e029e72fbec3fee3'),
